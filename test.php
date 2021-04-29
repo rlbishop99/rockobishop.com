d896aae8daec21b0e1e6a5353cdd0eedd6d4edfc
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-04-29 15:34:47 -0500

    Update test.php

--- test.php
+++ test.php
@@ -38,6 +38,7 @@ if(!isset($_SESSION['presses'])) {
                 header("Location: /test.php?presses=" . $_SESSION['presses']++);
                 break;
             default:
+                session_destroy();
                 header("Location: /test.php?presses=0");
                 break;
         }
