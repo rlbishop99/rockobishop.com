2470ea593bb5d562bafb21b2db187dfbb9c29ada
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-04-30 15:13:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
 }<?php 
     echo '<b><center> <span style="color: green; font-size: 50px;">Welcome to my site! </center></b>';
-    echo '<body style="background-color: magenta">';
+    echo '<body style="background-color: DarkSlateGray">';
 ?>
