ed05bab0344d4cb811ff7fff06b7c3af2ef46794
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-05-10 13:11:14 -0500

    Update test.php

--- test.php
+++ test.php
@@ -85,5 +85,11 @@ if(!isset($_SESSION['presses'])) {
     }
 </script>
 
-<iframe src="https://itch.io/embed-upload/2621668?color=f7eaa1" allowfullscreen="" width="1300" height="808" frameborder="0">
-<a href="https://plasmalot.itch.io/the-great-eggscape">Play The Great Eggscape on itch.io</a></iframe>
+<html>
+
+if(testBool == true)
+{
+    <iframe src="https://itch.io/embed-upload/2621668?color=f7eaa1" allowfullscreen="" width="1300" height="808" frameborder="0">
+    <a href="https://plasmalot.itch.io/the-great-eggscape">Play The Great Eggscape on itch.io</a></iframe>
+}
+<html>
