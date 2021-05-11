ab433cc643297b14dd578398e0f858155470e22d
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-05-11 13:37:57 -0500

    Update showcase.php

--- showcase.php
+++ showcase.php
@@ -101,6 +101,10 @@ body {
   background-size: 100% 100%;
 }
 
+.embeddedPhoto{
+  object-fit: contain;
+}
+
 /* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
 @media screen and (max-width: 700px) {
   .row {   
