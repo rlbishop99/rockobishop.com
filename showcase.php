b544031d0114cfe834de8b2571e55e813fa27435
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-05-11 13:53:16 -0500

    Update showcase.php

--- showcase.php
+++ showcase.php
@@ -206,10 +206,6 @@ body {
        Controlling my Pi from anywhere in the world means I can do Web Development anywhere, which is amazing.
     </p>
     <br>
-    <h2>TITLE HEADING</h2>
-    <h5>Title description, Sep 2, 2017</h5>
-    <p>Some text..</p>
-    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
   </div>
 </div>
 
