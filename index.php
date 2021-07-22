d14fb886bd2890d007f8414853b3cebf08d9a958
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-07-22 12:13:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php $light = "light.png"?>
-
 <html>
-    <head>
-        <table>
-            <tr>
-                <td>
-                    <?php echo "<ing src='images/$light'>"?>                \
-                </td>
-            </tr>
-    </body>
+<head>
+<title>Cog HP Calculator</title>
+</head>
+
+<body>
+<h1>Cog HP Calculator</h1>
+<table>
+    <tr>
+        <td>Bossbots</td>
+    </tr>
+    <tr>
+        <td>Lawbots></td>
+    </tr>
+    <tr>
+        <td>Cashbots</td>
+    </tr>
+    <tr>
+        <td>Sellbots</td>
+    </tr>
+</table>
+</body>
 </html>
