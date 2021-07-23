c39d3e78a9bc3274d597a54b1274ae86e9a6760a
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-07-23 11:11:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -37,36 +37,36 @@
     </tr>
     <tr>
         <td><b>Lawbots:</b></td>
-        <td><img src="images/lawbots/bottomfeeder.png"onclick=enableDiv(this.src)/><p class="text">Bottom Feeder</p></td>
-        <td><img src="images/lawbots/bloodsucker.png"onclick=enableDiv(this.src)/><p class="text">Bloodsucker</p></td>
-        <td><img src="images/lawbots/doubletalker.png"onclick=enableDiv(this.src)/><p class="text">Double Talker</p></td>
-        <td><img src="images/lawbots/ambulancechaser.png"onclick=enableDiv(this.src)/><p class="text">Ambulance Chaser</p></td>
-        <td><img src="images/lawbots/backstabber.png"onclick=enableDiv(this.src)/><p class="text">Back Stabber</p></td>
-        <td><img src="images/lawbots/spindoctor.png"onclick=enableDiv(this.src)/><p class="text">Spin Doctor</p></td>
-        <td><img src="images/lawbots/legaleagle.png"onclick=enableDiv(this.src)/><p class="text">Legal Eagle</p></td>
-        <td><img src="images/lawbots/bigwig.png"onclick=enableDiv(this.src)/><p class="text">BigWig</p></td>
+        <td><img src="images/lawbots/bottomfeeder.png"onclick="enableDiv(this.src)"/><p class="text">Bottom Feeder</p></td>
+        <td><img src="images/lawbots/bloodsucker.png"onclick="enableDiv(this.src)"/><p class="text">Bloodsucker</p></td>
+        <td><img src="images/lawbots/doubletalker.png"onclick="enableDiv(this.src)"/><p class="text">Double Talker</p></td>
+        <td><img src="images/lawbots/ambulancechaser.png"onclick="enableDiv(this.src)"/><p class="text">Ambulance Chaser</p></td>
+        <td><img src="images/lawbots/backstabber.png"onclick="enableDiv(this.src)"/><p class="text">Back Stabber</p></td>
+        <td><img src="images/lawbots/spindoctor.png"onclick="enableDiv(this.src)"/><p class="text">Spin Doctor</p></td>
+        <td><img src="images/lawbots/legaleagle.png"onclick="enableDiv(this.src)"/><p class="text">Legal Eagle</p></td>
+        <td><img src="images/lawbots/bigwig.png"onclick="enableDiv(this.src)"/><p class="text">BigWig</p></td>
     </tr>
     <tr>
         <td><b>Cashbots:</b></td>
-        <td><img src="images/cashbots/shortchange.png"onclick=enableDiv(this.src)/><p class="text">Short Change</p></td>
-        <td><img src="images/cashbots/pennypincher.png"onclick=enableDiv(this.src)/><p class="text">Penny Pincher</p></td>
-        <td><img src="images/cashbots/tightwad.png"onclick=enableDiv(this.src)/><p class="text">Tightwad</p></td>
-        <td><img src="images/cashbots/beancounter.png"onclick=enableDiv(this.src)/><p class="text">Bean Counter</p></td>
-        <td><img src="images/cashbots/numbercruncher.png"onclick=enableDiv(this.src)/><p class="text">Number Cruncher</p></td>
-        <td><img src="images/cashbots/moneybags.png"onclick=enableDiv(this.src)/><p class="text">Money Bags</p></td>
-        <td><img src="images/cashbots/loanshark.png"onclick=enableDiv(this.src)/><p class="text">Loan Shark</p></td>
-        <td><img src="images/cashbots/robberbaron.png"onclick=enableDiv(this.src)/><p class="text">Robber Baron</p></td>
+        <td><img src="images/cashbots/shortchange.png"onclick="enableDiv(this.src)"/><p class="text">Short Change</p></td>
+        <td><img src="images/cashbots/pennypincher.png"onclick="enableDiv(this.src)"/><p class="text">Penny Pincher</p></td>
+        <td><img src="images/cashbots/tightwad.png"onclick="enableDiv(this.src)"/><p class="text">Tightwad</p></td>
+        <td><img src="images/cashbots/beancounter.png"onclick="enableDiv(this.src)"/><p class="text">Bean Counter</p></td>
+        <td><img src="images/cashbots/numbercruncher.png"onclick="enableDiv(this.src)"/><p class="text">Number Cruncher</p></td>
+        <td><img src="images/cashbots/moneybags.png"onclick="enableDiv(this.src)"/><p class="text">Money Bags</p></td>
+        <td><img src="images/cashbots/loanshark.png"onclick="enableDiv(this.src)"/><p class="text">Loan Shark</p></td>
+        <td><img src="images/cashbots/robberbaron.png"onclick="enableDiv(this.src)"/><p class="text">Robber Baron</p></td>
     </tr>
     <tr>
         <td><b>Sellbots:</b></td>
-        <td><img src="images/sellbots/coldcaller.png"onclick=enableDiv(this.src)/><p class="text">Cold Caller</p></td>
-        <td><img src="images/sellbots/telemarketer.png"onclick=enableDiv(this.src)/><p class="text">Telemarketer</p></td>
-        <td><img src="images/sellbots/namedropper.png"onclick=enableDiv(this.src)/><p class="text">Name Dropper</p></td>
-        <td><img src="images/sellbots/gladhander.png"onclick=enableDiv(this.src)/><p class="text">Glad Hander</p></td>
-        <td><img src="images/sellbots/movershaker.png"onclick=enableDiv(this.src)/><p class="text">Mover & Shaker</p></td>
-        <td><img src="images/sellbots/twoface.png"onclick=enableDiv(this.src)/><p class="text">Two-Face</p></td>
-        <td><img src="images/sellbots/mingler.png"onclick=enableDiv(this.src)/><p class="text">The Mingler</p></td>
-        <td><img src="images/sellbots/hollywood.png"onclick=enableDiv(this.src)/><p class="text">Mr. Hollywood</p></td>
+        <td><img src="images/sellbots/coldcaller.png"onclick="enableDiv(this.src)"/><p class="text">Cold Caller</p></td>
+        <td><img src="images/sellbots/telemarketer.png"onclick="enableDiv(this.src)"/><p class="text">Telemarketer</p></td>
+        <td><img src="images/sellbots/namedropper.png"onclick="enableDiv(this.src)"/><p class="text">Name Dropper</p></td>
+        <td><img src="images/sellbots/gladhander.png"onclick="enableDiv(this.src)"/><p class="text">Glad Hander</p></td>
+        <td><img src="images/sellbots/movershaker.png"onclick="enableDiv(this.src)"/><p class="text">Mover & Shaker</p></td>
+        <td><img src="images/sellbots/twoface.png"onclick="enableDiv(this.src)"/><p class="text">Two-Face</p></td>
+        <td><img src="images/sellbots/mingler.png"onclick="enableDiv(this.src)"/><p class="text">The Mingler</p></td>
+        <td><img src="images/sellbots/hollywood.png"onclick="enableDiv(this.src)"/><p class="text">Mr. Hollywood</p></td>
     </tr>
 </table>
 
