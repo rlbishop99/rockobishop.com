03e4e5dea578dd34b56b2d43b285218d6141357a
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-07-23 13:32:05 -0500

    added calculateHP function

--- index.php
+++ index.php
@@ -75,20 +75,20 @@
 </table>
 
 <div id="testDiv" style="display: none; align-items: center; justify-content: center; margin-top: 50px;">
-        <select id="hpValues" onchange="this.previousElementSibling.value=this.value; this.previousElementSibling.focus()" style="margin-left: 200px;">
+        <select id="hpValues" onclick="calculateHP(value); this.previousElementSibling.value=this.value; this.previousElementSibling.focus()" style="margin-left: 200px;">
             <option selected> Select Cog Level</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-            <option>11</option>
-            <option>12</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
         </select>
 </div>
 
