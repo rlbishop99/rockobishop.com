8912c2e5dbcc6174fb9a5edf0a4e24276eab8f28
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-07-27 16:16:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -157,7 +157,7 @@ type="image/png"
         }
         else
         {
-            x.removeChild(x.childNodes[2]);
+            x.removeChild(x.childNodes[3]);
             x.appendChild(levelText);
             console.log("created and deleted text");
         }
