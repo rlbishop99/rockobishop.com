39a9bfbb19605ddde06f141d6f2de7aa573c4c62
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-08-27 13:29:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php echo "yeehaw bitch" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-G46Q3G89HY"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-G46Q3G89HY');
+    </script>
+
+</head>
+<body>
+    <?php echo "yeehaw bitch" ?>
+</body>
+</html>
