103c629ae93bfac7e060e1f487d9138d3f9101f0
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 20:08:46 -0600

    Added projects directory and eggscape button

--- projects.php
+++ projects.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
     <title>Projects</title>
-    <body>
+    <body background="#292929">
         <h1>Check out some of my completed projects below:</h1>
-        <button type="button" onclick="location.href='/heron.php'">Heron Formula: Triangle</button>
+        <button type="button" onclick="location.href='/Projects/heron.php'">Heron Formula: Triangle</button>
+        <button type="button" onclick="location.href='/Projects/eggscape.php'">The EGG Game</button>
     </body>
 </html>
