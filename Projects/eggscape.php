39545daac7d2ba71d5f07984bddf8478ac7444f4
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 21:21:58 -0600

    Edited eggscape and changed text color on projects

--- Projects/eggscape.php
+++ Projects/eggscape.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <title>The Great Eggscape</title>
-    <body style="background-color:#292929; text-align: center">
+    <body style="background-color:#292929; text-align: center;">
     <h1 style="padding: 0px 0px 20px 0px; color:#c8c8c8">The Great Eggscape</h1>
     <iframe src="https://itch.io/embed-upload/2621668?color=f7eaa1" allowfullscreen="" width="1300" height="808" frameborder="0"><a href="https://plasmalot.itch.io/the-great-eggscape">Play The Great Eggscape on itch.io</a></iframe>
     <h2 style="padding: 20px 0 0 0; color:#c8c8c8; max-width: 1300px; min-width: 100px; margin:0 auto;">This short physics-based game was developed over the course of 36 hours for a Game Jam. We were given a pallete of premade 1-bit art assets
