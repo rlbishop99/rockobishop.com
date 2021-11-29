9a5c3b0cf22a84b13e04a4355f4141b478d3a585
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 20:20:36 -0600

    Update eggscape.php
    Actually centered div

--- Projects/eggscape.php
+++ Projects/eggscape.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <title>The Great Eggscape</title>
-    <body style="background-color:#292929">
-    <iframe style="text-align: center" src="https://itch.io/embed-upload/2621668?color=f7eaa1" allowfullscreen="" width="1300" height="808" frameborder="0"><a href="https://plasmalot.itch.io/the-great-eggscape">Play The Great Eggscape on itch.io</a></iframe>
+    <body style="background-color:#292929; text-align: center">
+    <iframe src="https://itch.io/embed-upload/2621668?color=f7eaa1" allowfullscreen="" width="1300" height="808" frameborder="0"><a href="https://plasmalot.itch.io/the-great-eggscape">Play The Great Eggscape on itch.io</a></iframe>
     </body>
 </html>
