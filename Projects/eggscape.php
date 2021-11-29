5e467160a9e6d2db328fab734a3d48282bf9ef39
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 21:00:49 -0600

    Update eggscape.php
    Split text and added more background.

--- Projects/eggscape.php
+++ Projects/eggscape.php
@@ -7,9 +7,12 @@
     <h2 style="padding: 20px 0 0 0; color:#c8c8c8; max-width: 1300px; min-width: 100px; margin:0 auto;">This short physics-based game was developed over the course of 36 hours for a Game Jam. We were given a pallete of premade 1-bit art assets
         and were told to use those assets exclusively to create the most fun/creative game that we could imagine. I decided to make a game about a chicken
         egg that was trying to "eggscape" the chicken coop out of fear of being eaten. The egg moves with a physics body and moves kinetically through impulses.
+    </h2>
+    <h2 style="padding: 20px 0 0 0; color:#c8c8c8; max-width: 1300px; min-width: 100px; margin:0 auto;">
         It was the first Game Jam I had ever participated in, and it was the first time I had made a full game that was playable from start to finish.
+        There are absolutely some issues: I naively tied physics to frame rate/refresh rate, so some of the physics make the game unplayable if you aren't playing on a 60hz monitor. (Which, let's be honest, most of us aren't at this point!)
         I certainly learned a lot while making it, it taught me a lot about debugging, code review, creativity, and good code optimization practices.
-        I wouldn't try to argue that this game is GOOD, but it is playable and should be (hopefully) bug-free! Have fun!
+        I wouldn't try to argue that this game is GOOD, but it is playable (on 60hz monitors) and should be bug-free! Have fun!
     </h2>
     </body>
 </html>
