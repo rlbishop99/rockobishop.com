2b4ac96caf92370fd668eb0ab9f252742edeee06
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 20:12:21 -0600

    Added Great Eggscape embed

---
+++ Projects/eggscape.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <title>The Great Eggscape</title>
+    <body style="background-color:#292929">
+    <iframe src="https://itch.io/embed-upload/2621668?color=f7eaa1" allowfullscreen="" width="1300" height="808" frameborder="0"><a href="https://plasmalot.itch.io/the-great-eggscape">Play The Great Eggscape on itch.io</a></iframe>
+    </body>
+</html>
--- projects.php
+++ projects.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <title>Projects</title>
-    <body background="#292929">
+    <body style="background-color:#292929">
         <h1>Check out some of my completed projects below:</h1>
         <button type="button" onclick="location.href='/Projects/heron.php'">Heron Formula: Triangle</button>
         <button type="button" onclick="location.href='/Projects/eggscape.php'">The EGG Game</button>
