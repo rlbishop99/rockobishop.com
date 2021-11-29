571636608533f3b0a174633fa0d151cac54df486
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 20:05:53 -0600

    Update heron.php

--- heron.php
+++ heron.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html background="#292929">
     <head>
         <title>Heron Formula: Triangle</title>
     </head>
-    <body background="#292929">
+    <body>
         <div data-pym-src="https://www.jdoodle.com/embed/v0/47DH"></div>
         <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js" type="text/javascript"></script>
     </body>
