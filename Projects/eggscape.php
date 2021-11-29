f2e528ca0f87c89ea44f05062534293819112343
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 20:30:41 -0600

    Update eggscape.php
    Added text padding and changed color to improve readability

--- Projects/eggscape.php
+++ Projects/eggscape.php
@@ -2,8 +2,8 @@
 <html>
     <title>The Great Eggscape</title>
     <body style="background-color:#292929; text-align: center">
-    <h1>The Great Eggscape</h1>
-    <h2 style="padding: 0px 0px 20px 0px">This short physics-based game was developed over the course of 36 hours for a Game Jam. We were given a pallete of premade 1-bit art assets
+    <h1 style="padding: 0px 0px 20px 0px; color:#c8c8c8">The Great Eggscape</h1>
+    <h2 style="padding: 0px 0px 20px 0px; color:#c8c8c8">This short physics-based game was developed over the course of 36 hours for a Game Jam. We were given a pallete of premade 1-bit art assets
         and were told to use those assets exclusively to create the most fun/creative game that we could imagine. I decided to make a game about a chicken
         egg that was trying to "eggscape" the chicken coop out of fear of being eaten. The egg moves with a physics body and moves kinetically through impulses.
         It was the first Game Jam I had ever participated in, and it was the first time I had made a full game that was playable from start to finish.
