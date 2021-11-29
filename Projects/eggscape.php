e97a36be2d42d430b4250c3d5de18c02710cbbdf
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 20:53:56 -0600

    Update eggscape.php

--- Projects/eggscape.php
+++ Projects/eggscape.php
@@ -4,7 +4,7 @@
     <body style="background-color:#292929; text-align: center">
     <h1 style="padding: 0px 0px 20px 0px; color:#c8c8c8">The Great Eggscape</h1>
     <iframe src="https://itch.io/embed-upload/2621668?color=f7eaa1" allowfullscreen="" width="1300" height="808" frameborder="0"><a href="https://plasmalot.itch.io/the-great-eggscape">Play The Great Eggscape on itch.io</a></iframe>
-    <h2 style="padding: 20px 0 0 0; color:#c8c8c8; display:inline">This short physics-based game was developed over the course of 36 hours for a Game Jam. We were given a pallete of premade 1-bit art assets
+    <h2 style="padding: 20px 0 0 0; color:#c8c8c8; max-width: 350px; min-width: 100px; text-align:center;">This short physics-based game was developed over the course of 36 hours for a Game Jam. We were given a pallete of premade 1-bit art assets
         and were told to use those assets exclusively to create the most fun/creative game that we could imagine. I decided to make a game about a chicken
         egg that was trying to "eggscape" the chicken coop out of fear of being eaten. The egg moves with a physics body and moves kinetically through impulses.
         It was the first Game Jam I had ever participated in, and it was the first time I had made a full game that was playable from start to finish.
