4634d821682557a7dec3c84ad267bc8c1585862d
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 21:23:47 -0600

    Update projects.php

--- projects.php
+++ projects.php
@@ -14,9 +14,9 @@
 
 
     <title>Projects</title>
-    <body style="background-color:#292929">
-        <h1 style="color:#c8c8c8; text-align:center;">Check out some of my completed projects below:</h1>
-        <button type="button" onclick="location.href='/Projects/heron.php'">Heron Formula: Triangle</button>
+    <body style="background-color:#292929; text-align:center;">
+        <h1 style="color:#c8c8c8;">Check out some of my completed projects below:</h1>
+        <button type="button" onclick="location.href='/Projects/heron.php'" style="padding: 0 50 0 0;">Heron Formula: Triangle</button>
         <button type="button" onclick="location.href='/Projects/eggscape.php'">The EGG Game</button>
     </body>
 </html>
