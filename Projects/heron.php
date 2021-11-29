c952db1818f37233c2014647bebb83eea14cd8bb
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2021-11-28 20:28:04 -0600

    Added description to Eggscape and changed background colors

--- Projects/heron.php
+++ Projects/heron.php
@@ -3,7 +3,7 @@
     <head>
         <title>Heron Formula: Triangle</title>
     </head>
-    <body>
+    <body style="text-align: center; background-color:#292929">
         <div data-pym-src="https://www.jdoodle.com/embed/v0/47DH"></div>
         <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js" type="text/javascript"></script>
     </body>
