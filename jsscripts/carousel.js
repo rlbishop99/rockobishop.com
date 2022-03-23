9f93161c5713bd8bec447f349ce68eff9aa99a2c
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2022-03-23 15:25:38 -0500

    Update carousel.js

--- jsscripts/carousel.js
+++ jsscripts/carousel.js
@@ -4,7 +4,7 @@ const pause = 1200; // <--- the longer delay between text direction changes
 
 function typeWriter(i=0, index=1, direction=1) {
   let displayed = texts[i].slice(0, index);
-  document.querySelector("#hats").textContent = displayed;
+  document.querySelector('hats').textContent = displayed;
 
   if (displayed.length >= texts[i].length) { // start removing after pause
     setTimeout(() => typeWriter(i, index-1, -1), pause);
