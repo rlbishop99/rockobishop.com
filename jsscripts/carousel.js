3fef6ba9ad022b8d601e86101463c0c66fe2ee84
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2022-03-23 15:50:00 -0500

    FIFTH CAROUSEL ATTEMPT

--- jsscripts/carousel.js
+++ jsscripts/carousel.js
@@ -1,39 +1,37 @@
-const words = ["Game Developer.", "Web Developer.", "Software Engineer.", "Computer Scientist."];
-let i = 0;
-let timer;
+var speed = 50;
+var speed2 = 100;
+var str = document.getElementById('roles');
+var i = 0;
+var isRemoving = false;
 
-function typingEffect() {
-	let word = words[i].split("");
-	var loopTyping = function() {
-		if (word.length > 0) {
-			document.getElementById('word').innerHTML += word.shift();
-		} else {
-			deletingEffect();
-			return false;
-		};
-		timer = setTimeout(loopTyping, 500);
-	};
-	loopTyping();
-};
+var messages = [
+    "Web Developer.",
+    "Game Developer.",
+    "Software Engineer.",
+    "Computer Scientist."
+]
 
-function deletingEffect() {
-	let word = words[i].split("");
-	var loopDeleting = function() {
-		if (word.length > 0) {
-			word.pop();
-			document.getElementById('word').innerHTML = word.join("");
-		} else {
-			if (words.length > (i + 1)) {
-				i++;
-			} else {
-				i = 0;
-			};
-			typingEffect();
-			return false;
-		};
-		timer = setTimeout(loopDeleting, 200);
-	};
-	loopDeleting();
-};
+function action() {
+  if (isRemoving) {
+    if (str.innerText.length > 0) {
+      str.innerText = str.innerText.substr(0, str.innerHTML.length - 1);
+      setTimeout( action, speed2 );
+      return;
+    }
+    isRemoving = false;
+    i++;
+    if (i >= messages.length) {
+      i = 0;
+    }
+    setTimeout( action, speed );
+    return;
+  }
+  var message = messages[i];
+  str.innerText = message.substr(0, str.innerHTML.length + 1);
+  if (str.innerText.length === message.length) {
+    isRemoving = true;
+  }
+  setTimeout( action, isRemoving ? speed2 : speed );
+}
 
-typingEffect();
+setTimeout( action, speed ) ;
